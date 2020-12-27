a142096365caaea477c5253708878c34c2552bf8
Sotiris Blad <sotiris2009@hotmail.com>
2020-12-27 21:41:17 +0200

    test

--- BTCPayServer.Common/Altcoins/Ethereum/BTCPayNetworkProvider.Ethereum.cs
+++ BTCPayServer.Common/Altcoins/Ethereum/BTCPayNetworkProvider.Ethereum.cs
@@ -51,24 +51,21 @@ namespace BTCPayServer
             {
                 Add(new ERC20BTCPayNetwork()
                 {
-                    CryptoCode = "USDT20",
-                    DisplayName = "Tether USD (ERC20)",
+                    CryptoCode = "FAU",
+                    DisplayName = "Faucet Token",
                     DefaultRateRules = new[]
                     {
-                        "USDT20_UST = 1",
-                        "USDT20_X = USDT20_BTC * BTC_X",
-                        "USDT20_BTC = bitfinex(UST_BTC)",
+                        "FAU_X = FAU_BTC * BTC_X",
+                        "FAU_BTC = 0.01",
                     },
-                    BlockExplorerLink =
-                        NetworkType == NetworkType.Mainnet
-                            ? "https://etherscan.io/address/{0}#tokentxns"
-                            : "https://ropsten.etherscan.io/address/{0}#tokentxns",
-                    CryptoImagePath = "/imlegacy/liquid-tether.svg",
+                    BlockExplorerLink = "https://ropsten.etherscan.io/address/{0}#tokentxns",
                     ShowSyncSummary = false,
-                    CoinType = NetworkType == NetworkType.Mainnet? 60 : 1,
-                    ChainId = NetworkType == NetworkType.Mainnet ? 1 : 3,
-                    SmartContractAddress = "0xdAC17F958D2ee523a2206206994597C13D831ec7",
-                    Divisibility = 6
+                    CoinType =  1,
+                    ChainId =  3,
+                    //use https://erc20faucet.com for testnet
+                    SmartContractAddress = "0xFab46E002BbF0b4509813474841E0716E6730136",
+                    Divisibility = 18,
+                    CryptoImagePath = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=",
                 });
             }
             
